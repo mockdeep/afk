cf2af59113a5e4f204e1528fc4a80e020081eb0b
Robert Fletcher <lobatifricha@gmail.com>
2016-11-19 13:23:17 -0800

    re-group spec helper stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,18 +3,19 @@ require_relative '../lib/afk'
 
 require 'vcr'
 
-RSpec.configuration.after { AFK.reset }
+AFK::Configuration.loaded_config[:trello] = {
+  board_id: 'ui1o0UsE',
+  project_list_name: 'not a project',
+  developer_public_key: ENV.fetch('TRELLO_DEVELOPER_PUBLIC_KEY'),
+  member_token: ENV.fetch('TRELLO_MEMBER_TOKEN'),
+}
 
-trello = AFK::Configuration.loaded_config.fetch(:trello)
-trello[:board_id] = 'ui1o0UsE'
-trello[:developer_public_key] ||= ENV.fetch('TRELLO_DEVELOPER_PUBLIC_KEY')
-trello[:member_token] ||= ENV.fetch('TRELLO_MEMBER_TOKEN')
+RSpec.configure do |config|
+  config.after { AFK.reset }
+  config.filter_run_when_matching :focus
+end
 
 VCR.configure do |config|
   config.cassette_library_dir = 'tmp/vcr_cassettes'
   config.hook_into :webmock
-end
-
-RSpec.configure do |config|
-  config.filter_run_when_matching :focus
 end
